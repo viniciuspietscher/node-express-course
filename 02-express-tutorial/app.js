c6e57727d4f0de4fdddf3798ba587671398be71a
Vinicius Pietscher <vini.914@gmail.com>
2022-07-24 20:21:44 -0400

    Week04 (#4)
    * week04

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,53 +1,23 @@
 const express = require("express")
 const app = express()
+
+const people = require("./routes/people")
+const auth = require("./routes/auth")
+
 const PORT = 5000
-const { products } = require("./data")
 
-app.get("/", (req, res) => {
-  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
-})
+// static assets
+app.use(express.static("./methods-public"))
 
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product
-    return { id, name, image }
-  })
-  res.json(newProducts)
-})
+// parse form data
+app.use(express.urlencoded({ extended: "false" }))
 
-app.get("/api/products/:id", (req, res) => {
-  const singleProduct = products.find(
-    (product) => product.id === Number(req.params.id)
-  )
-  if (!singleProduct) {
-    res.send("product does not exist")
-  }
-  res.json(singleProduct)
-})
+// parse json
+app.use(express.json())
 
-app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
-  console.log(req.params)
-  res.send("hello")
-})
-
-app.get("/api/v1/query", (req, res) => {
-  const { search, limit } = req.query
-  let sortedProducts = [...products]
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) =>
-      product.name.startsWith(search)
-    )
-  }
-  if (limit) {
-    singleProduct = sortedProducts.slice(0, Number(limit))
-  }
-  if (sortedProducts.length < 1) {
-    return res.status(200).json({ success: true, data: [] })
-  }
-  return res.status(200).json(sortedProducts)
-})
+app.use("/api/people", people)
+app.use("/login", auth)
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`)
+  console.log(`Server listening on port ${PORT}`)
 })
