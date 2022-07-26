b904eb1437313e4df9d2d9754fea072ac19ba679
Vinicius Pietscher <vini.914@gmail.com>
2022-07-26 19:05:33 -0400

    week06

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,18 +1,20 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
-require("dotenv").config()
 const tasks = require("./routes/tasks")
 const connectDB = require("./db/connect")
+const notFound = require("./middleware/not-found")
+const errorHandler = require("./middleware/error-handler")
+
 const PORT = process.env.PORT || 3000
 
 // middleware
+app.use(express.static("./public"))
 app.use(express.json())
 
 app.use("/api/v1/tasks", tasks)
-
-app.get("/hello", (req, res) => {
-  res.send("Task manager app")
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const start = async () => {
   try {
