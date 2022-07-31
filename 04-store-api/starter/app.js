a9608da6d969bc87e53c5333b62c6c891afb41a1
Vinicius Pietscher <vini.914@gmail.com>
2022-07-30 21:51:23 -0400

    week 07

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,37 @@
-console.log('04 Store API')
+require("dotenv").config()
+require("express-async-errors")
+const connectDB = require("./db/connect")
+const productsRouter = require("./routes/products")
+const PORT = process.env.PORT || 3000
+
+const express = require("express")
+const app = express()
+
+const notFoundMiddleware = require("./middleware/not-found")
+const errorMiddleware = require("./middleware/error-handler")
+
+// middleware
+app.use(express.json())
+
+// routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+app.use("/api/v1/products", productsRouter)
+
+// products route
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const start = async () => {
+  try {
+    // connect DB
+    connectDB(process.env.MONGO_URI)
+    app.listen(PORT, console.log(`Server is listening on port ${PORT}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
